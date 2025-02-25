959eef42b3c2503d7fdc05ea922627949024e9f4
alinacozy <alinakoroleva2307@gmail.com>
2025-02-25 17:23:36 +0400

    add base springboot app

---
+++ src/main/java/com/example/recipes_helper/RecipesHelperApplication.java
@@ -0,0 +1,25 @@
+package com.example.recipes_helper;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class RecipesHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesHelperApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String getMethodName(@RequestParam(defaultValue = "Alina and Sofia") String name) {
+		return String.format("Hello, %s!", name);
+	}
+
+	
+}
---
+++ src/test/java/com/example/recipes_helper/RecipesHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipes_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
