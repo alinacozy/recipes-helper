747e7c4faa07642f4ed2c42e98db8cf794ce5974
alinacozy <alinakoroleva2307@gmail.com>
2025-03-19 19:47:11 +0400

    add /recipes

--- src/main/java/com/example/recipes_helper/RecipesHelperApplication.java
+++ src/main/java/com/example/recipes_helper/RecipesHelperApplication.java
@@ -1,6 +1,8 @@
 package com.example.recipes_helper;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.SpringApplication;
@@ -31,8 +33,29 @@ public class RecipesHelperApplication {
 	  return String.format("Hello, %s!", name);
 	}
 
+	@GetMapping("/recipes")
+	public String getRecipes() {
+	
+	  List<Recipe> recipes = (List<Recipe>) repository.findAll();
+	  String result="";
+	  for (Recipe r : recipes){
+		result=result.concat(String.format("Название: %s. Категория: %s. Описание: %s.\n", r.getRecipeName(), r.getRecipeCategory(), r.getDescription()));
+	  }
+	  return result;
+	}
 
-	@GetMapping("/recipe/{idRecipe}")
+	// @GetMapping("/recipes")
+	// public String getRecipes(@RequestParam(defaultValue = "") String recipeCategory, @RequestParam(defaultValue = "") String productCategory) {
+	
+	//   List<Recipe> recipes = (List<Recipe>) repository.findAll();
+	//   String result="";
+	//   for (Recipe r : recipes){
+	// 	result=result.concat(String.format("Название: %s. Категория: %s. Описание: %s.\n", r.getRecipeName(), r.getRecipeCategory(), r.getDescription()));
+	//   }
+	//   return result;
+	// }
+
+	@GetMapping("/recipes/{idRecipe}")
 	public String getRecipe(@PathVariable(name = "idRecipe") Long idRecipe) {
 	
 	  Recipe recipe = repository.findByRecipeId(idRecipe);
