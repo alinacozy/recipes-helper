d76d04c831f80e3e14ba66e94e00517ece439b2e
alinacozy <alinakoroleva2307@gmail.com>
2025-04-16 17:35:53 +0400

    remove (commented out) SQL query

--- src/main/java/com/example/recipes_helper/repository/RecipeRepository.java
+++ src/main/java/com/example/recipes_helper/repository/RecipeRepository.java
@@ -2,9 +2,7 @@ package com.example.recipes_helper.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.example.recipes_helper.model.Recipe;
 import com.example.recipes_helper.model.RecipeCategory;
@@ -16,13 +14,13 @@ public interface RecipeRepository extends CrudRepository<Recipe, Long>{
 
   Recipe findByRecipeId(Long recipeId);
     
-  @Query(value="select r.recipe_id, r.recipe_name, r.recipe_category, r.description from \"recipes-helper-db\".recipe r\n" + //
-        "where (SELECT count(*) FROM \"recipes-helper-db\".user_product up\n" + //
-        "inner join \"recipes-helper-db\".users u using(user_id)\n" + //
-        "inner join \"recipes-helper-db\".list_product lp using(product_id)\n" + //
-        "where u.user_id = :userId and up.count>=lp.count and lp.recipe_id = r.recipe_id ) =\n" + //
-        "(SELECT count(*) FROM \"recipes-helper-db\".list_product lp \n" + //
-        "where lp.recipe_id = r.recipe_id)", nativeQuery = true)
-  List <Recipe> findByUser(@Param("userId") Long userId);
+  // @Query(value="select r.recipe_id, r.recipe_name, r.recipe_category, r.description from \"recipes-helper-db\".recipe r\n" + //
+  //       "where (SELECT count(*) FROM \"recipes-helper-db\".user_product up\n" + //
+  //       "inner join \"recipes-helper-db\".users u using(user_id)\n" + //
+  //       "inner join \"recipes-helper-db\".list_product lp using(product_id)\n" + //
+  //       "where u.user_id = :userId and up.count>=lp.count and lp.recipe_id = r.recipe_id ) =\n" + //
+  //       "(SELECT count(*) FROM \"recipes-helper-db\".list_product lp \n" + //
+  //       "where lp.recipe_id = r.recipe_id)", nativeQuery = true)
+  // List <Recipe> findByUser(@Param("userId") Long userId);
 } 
 
